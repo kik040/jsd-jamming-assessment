635460a76560d8ee6dff4037e344ba9da989c854
Sukanya Kaewbor <sukanya.kaewbor@gmail.com>
2022-03-18 12:18:56 +0700

    change URI

--- src/utils/Spotify.js
+++ src/utils/Spotify.js
@@ -1,7 +1,7 @@
 // TODO: Get Client ID from https://developer.spotify.com/dashboard/ and put it here
 const clientId = '88f93de7a9024bc390a469d5be5049ca';
 
-const redirectUri = 'http://localhost:3000/';
+const redirectUri = 'https://jsd-jamming.netlify.app/';
 const spotifyUrl = `https://accounts.spotify.com/authorize?response_type=token&scope=playlist-modify-public&client_id=${clientId}&redirect_uri=${redirectUri}`;
 let accessToken = undefined;
 let expiresIn = undefined;
